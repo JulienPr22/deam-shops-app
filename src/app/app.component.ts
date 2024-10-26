318b843d84576176f61fb056509e984971bd6bd6
Julien Perrier <perrierjulien22@gmail.com>
2024-10-26 10:13:28 +0200

    init eslint and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductCardComponent } from "./components/product-card/product-card.component";
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ProductCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dream-shops-app';
