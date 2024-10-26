4555de2d7b8c8706510448f2a1a08945b41ba7b8
Julien Perrier <perrierjulien22@gmail.com>
2024-10-26 09:39:33 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,scss,ts}'], // Ajoute .scss ici
+  darkMode: false, // ou 'media' ou 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
